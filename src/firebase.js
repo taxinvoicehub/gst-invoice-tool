5310745f64efcc83651c70e7b434de6824baa2b6
taxinvoicehub <vinaymahatkar5@gmail.com>
2026-06-21 00:52:34 +0530

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCNqeHVscgqUhEOzkDoqtvirvWaXfMkBCw",
+  authDomain: "gst-invoice-tool-478db.firebaseapp.com",
+  databaseURL: "https://gst-invoice-tool-478db-default-rtdb.firebaseio.com",
+  projectId: "gst-invoice-tool-478db",
+  storageBucket: "gst-invoice-tool-478db.firebasestorage.app",
+  messagingSenderId: "452197123231",
+  appId: "1:452197123231:web:dc3c8c4d3ad6066a9d0eaa",
+  measurementId: "G-SX28ZRRN88"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
